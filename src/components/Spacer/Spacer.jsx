ac891854c9c0737f6817e3ae8cf657aa3e6037fa
徐涛 <midnite@outlook.com>
2023-12-28 16:06:50 +0800

    enhance(compo):增强占位组件接受额外样式的能力。

--- src/components/Spacer/Spacer.jsx
+++ src/components/Spacer/Spacer.jsx
@@ -1,6 +1,21 @@
+import cx from "clsx";
 import { prop } from "ramda";
 import classes from "./Spacer.module.css";
 
-export default function Spacer() {
-  return <div className={prop("spacer", classes)}></div>;
+/**
+ * @typedef {import("react").HTMLAttributes["className"]} ClassName
+ *
+ * @typedef {Object} SpacerProps
+ * @property {ClassName} [className]
+ */
+
+/**
+ * @param {SpacerProps} props
+ */
+export default function Spacer(props) {
+  return (
+    <div
+      className={cx(prop("spacer", classes), prop("className", props))}
+    ></div>
+  );
 }

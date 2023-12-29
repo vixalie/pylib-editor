5043d3aab2a2d59f029d8f01a89e80538da43f47
徐涛 <midnite@outlook.com>
2023-12-29 17:16:22 +0800

    enhance(page):调整创建词库界面。

--- src/pages/NewLibrary/NewLibrary.jsx
+++ src/pages/NewLibrary/NewLibrary.jsx
@@ -1,4 +1,5 @@
 import Button from "@/components/Button";
+import Input from "@/components/Input";
 import Spacer from "@/components/Spacer";
 import ContentLayout from "@/layout/ContentLayout";
 import { prop } from "ramda";
@@ -8,7 +9,9 @@ import classes from "./NewLibrary.module.css";
 export default function NewLibraray() {
   const { register, handleSubmit } = useForm();
 
-  const onSubmit = (data) => {};
+  const onSubmit = (data) => {
+    console.log("Form Submit", data);
+  };
 
   return (
     <ContentLayout title="新建词库">
@@ -18,15 +21,20 @@ export default function NewLibraray() {
       >
         <div className={prop("form-line", classes)}>
           <label htmlFor="name">词库名称</label>
-          <input
-            type="text"
+          <Input.Text
             {...register("name")}
+            variant="underline"
             style={{ maxWidth: "15em" }}
           />
         </div>
         <div className={prop("form-line", classes)}>
           <label htmlFor="location">存储位置</label>
-          <input type="text" {...register("location")} readOnly />
+          <Input.Text
+            {...register("location")}
+            variant="underline"
+            className={prop("input", classes)}
+            readOnly
+          />
           <Button type="button">选择存储位置</Button>
         </div>
         <div className={prop("form-line", classes)}>
